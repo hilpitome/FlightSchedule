f552f4098172e2101fd30357025ec2f6bf51d9a6
hilpitome <hilpitome@gmail.com>
2019-07-30 15:53:38 +0300

    fetch endpoint for schedules

--- app/src/main/java/com/workshop/hilpitome/flightscheduler/networking/LufthansaNetworkApi.java
+++ app/src/main/java/com/workshop/hilpitome/flightscheduler/networking/LufthansaNetworkApi.java
@@ -3,6 +3,7 @@ package com.workshop.hilpitome.flightscheduler.networking;
 import com.workshop.hilpitome.flightscheduler.model.AirportsResponse;
 import com.workshop.hilpitome.flightscheduler.model.AuthResponse;
 import com.workshop.hilpitome.flightscheduler.model.LoginData;
+import com.workshop.hilpitome.flightscheduler.model.SchedulesResponse;
 
 import retrofit2.Response;
 import retrofit2.http.Body;
@@ -12,6 +13,8 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 import rx.Observable;
 
 public interface LufthansaNetworkApi {
@@ -31,7 +34,11 @@ public interface LufthansaNetworkApi {
     Observable<Response<AirportsResponse>> fetchAirports(@Header("Authorization") String accessToken);
 
 
-//    @Headers("Accept: application/json")
-//    @GET("operations/schedules/{origin}/{destination}/{fromDateTime}?directFlights=true]")
+    @Headers("Accept: application/json")
+    @GET("operations/schedules/{origin}/{destination}/{fromDateTime}")
+    Observable<Response<SchedulesResponse>> fetchSchedules(@Header("Authorization") String accessToken,
+                                                           @Path("origin") String origin,
+                                                           @Path("destination") String destination,
+                                                           @Query("directFlights") String bool);
 }
 
